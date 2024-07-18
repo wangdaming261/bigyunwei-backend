3d4a70e6a2f5526bc31f8becd97834bce3632a86
zk <1@qq.com>
2024-07-18 17:01:20 +0800

    12.8

--- src/web/middleware/global_config.go
+++ src/web/middleware/global_config.go
@@ -1,6 +1,8 @@
 package middleware
 
-import "github.com/gin-gonic/gin"
+import (
+	"github.com/gin-gonic/gin"
+)
 
 func ConfigMiddleware(m map[string]interface{}) gin.HandlerFunc {
 	return func(c *gin.Context) {
@@ -10,3 +12,17 @@ func ConfigMiddleware(m map[string]interface{}) gin.HandlerFunc {
 		c.Next()
 	}
 }
+
+//func TimeCost() gin.HandlerFunc {
+//	return func(c *gin.Context) {
+//		//请求前获取当前时间
+//		nowTime := time.Now()
+//		//请求处理
+//		c.Next()
+//		sc := c.MustGet(common.GIN_CTX_CONFIG_CONFIG).(*config.ServerConfig)
+//		sc.Logger.Info("耗时中间件打印结果",
+//			zap.String("url", c.Request.URL.String()),
+//			zap.Duration("耗时", time.Since(nowTime)),
+//		)
+//	}
+//}
--- src/web/view/router.go
+++ src/web/view/router.go
@@ -1,21 +1,12 @@
 package view
 
 import (
-	"time"
-
 	"github.com/gin-gonic/gin"
 )
 
 func ConfigRoutes(r *gin.Engine) {
 	base := r.Group("/basic-api")
 	{
-		base.GET("/ping", ping)
-		base.GET("/now", now)
 		base.POST("/login", UserLogin)
 	}
-}
-
-func now(r *gin.Context) {
-	//time.Sleep(3 * time.Second)
-	r.String(200, time.Now().Format("2006-01-02 15:04:05"))
 }
